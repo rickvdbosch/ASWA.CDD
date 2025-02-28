52eda34f2f575fce199b75108d60683020b33df5
Rick van den Bosch <rickvdbosch@outlook.com>
2025-02-28 08:33:54 +0100

    Application Insights

--- ASWA.CCD.Web/Program.cs
+++ ASWA.CCD.Web/Program.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Azure.Identity;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
